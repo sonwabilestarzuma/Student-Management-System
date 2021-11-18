241a3b02758d4b526654ff2779f4c6fb5f686482
GlobeScope <sonwabilestarzuma@gmail.com>
2021-11-18 10:18:14 +0200

    Add project files.

---
+++ Student-Management-System/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,15 @@
+﻿using Student_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management_System.SchoolViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
